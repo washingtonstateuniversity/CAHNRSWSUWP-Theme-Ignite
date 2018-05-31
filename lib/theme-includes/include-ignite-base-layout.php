e62eaa37cbc33a6688083df17dbe3368c58b8e4e
danialbleile <danbleile@gmail.com>
2018-05-31 16:04:03 -0700

    Added Base classes for breadcrumbs and layout

---
+++ lib/theme-includes/include-ignite-base-layout.php
@@ -0,0 +1,9 @@
+<?php
+
+class Ignite_Base_Layout {
+
+	public function __construct() {
+
+	} // end __construct
+
+}
---
+++ lib/theme-includes/include-ignite-breadcrumbs.php
@@ -0,0 +1,10 @@
+<?php
+
+class Ignite_Breadcrumbs {
+
+
+	public function __construct() {
+
+	} // End __construct
+
+} // End Ignite_Breadcrumb
