ef285b60764f0ab188fcc754923a4c9f5dc03079
danialbleile <danbleile@gmail.com>
2018-06-07 08:26:21 -0700

    Change esc_html to esc_attr in dynamic-scroll banner

--- includes/banners/types/dynamic-scroll.php
+++ includes/banners/types/dynamic-scroll.php
@@ -1,5 +1,5 @@
 <?php if ( ! empty( $banner_image ) ) : ?>
-<div id="site-banner" class="static-image-wide-banner parallax-banner<?php if ( ! empty( $classes ) ) : ?> <?php echo esc_html( implode( ' ', $classes ) ); ?><?php endif; ?>">
+<div id="site-banner" class="static-image-wide-banner parallax-banner<?php if ( ! empty( $classes ) ) : ?> <?php echo esc_attr( implode( ' ', $classes ) ); ?><?php endif; ?>">
 	<div class="banner-image-wrap" style="background-image:url(<?php echo esc_url( $banner_image ) ?>);">
 		<div class="banner-image" style="background-image:url(<?php echo esc_url( $banner_image ) ?>);">
 		</div>
