cb791cb1b2c5edd2c3ed1d4b9107840e3553f8f6
danialbleile <danial.bleile@wsu.edu>
2018-10-10 15:31:19 -0700

    Fix banner Image

--- theme-parts/page-banners/displays/title-banner.php
+++ theme-parts/page-banners/displays/title-banner.php
@@ -1,5 +1,5 @@
 <div class="ignite-title-banner">
-	<div class="ignite-banner-background unbound recto verso" style="background-image:url(<?php echo esc_url( $img ); ?>)">
+	<div class="ignite-banner-background unbound recto verso" style="background-image:url(<?php echo esc_url( $img ); g?>)">
 	</div>
 	<div class="ignite-banner-content" style="min-height:<?php echo esc_attr( $height ); ?>;">
 		<div class="ignite-banner-content-inner">
