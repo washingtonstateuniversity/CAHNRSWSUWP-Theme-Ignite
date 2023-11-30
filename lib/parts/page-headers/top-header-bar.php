35a25141cec1129a3cd520bf75cd498e4aae87de
Efren Vasquez <efren.vasquez@wsu.edu>
2023-11-30 09:30:08 -0600

    Update Request Info Link in the header

--- lib/parts/page-headers/top-header-bar.php
+++ lib/parts/page-headers/top-header-bar.php
@@ -13,7 +13,7 @@
 	</nav><nav class="top-header-bar-secondary-nav">
 		<?php if ( is_active_sidebar( 'global-top-header-bar-secondary' ) ) : dynamic_sidebar( 'global-top-header-bar-secondary' ); ?>
 	   	<?php else: ?>
-		<a href="http://admission.wsu.edu/applications/index.html">Apply</a><a href="http://cahnrs.wsu.edu/academics/prospective/">Request Info</a><a href="https://foundation.wsu.edu/give/?fund=c39b3693-1ede-4044-aa2a-f5fed2b62638&utm_source=college-of-agricultural-human-and-natural-resource-sciences-excellence-fund&utm_medium=wsu-link&utm_campaign=agricultural-human-and-natural-resource-sciences">Give</a>
+		<a href="http://admission.wsu.edu/applications/index.html">Apply</a><a href="https://admission.wsu.edu/connect/request-information/">Request Info</a><a href="https://foundation.wsu.edu/give/?fund=c39b3693-1ede-4044-aa2a-f5fed2b62638&utm_source=college-of-agricultural-human-and-natural-resource-sciences-excellence-fund&utm_medium=wsu-link&utm_campaign=agricultural-human-and-natural-resource-sciences">Give</a>
 		<?php endif; ?>
 	</nav>
 </div>
